f3f298551b488cc5615165296d7becba1331a3cd
JesusRules <36013969+JesusRules@users.noreply.github.com>
2023-08-17 15:17:22 -0400

    Found solution

--- js/app.js
+++ js/app.js
@@ -4,15 +4,8 @@ let xValue = 0, yValue = 0;
 
 let rotateDegree = 0; //mountains
 
-addEventListener("mousemove", (event) => {
-    xValue = event.clientX - innerWidth / 2; //checks how far from middle
-    yValue = event.clientY - innerHeight / 2; //checks how far from middle
-
-    rotateDegree = (xValue / (innerWidth / 2)) * 20;
-
-    console.log(rotateDegree);
-    
-    // console.log(xValue, yValue);
+function update(cursorPosition) 
+{
     parallax_el.forEach(el => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
@@ -20,9 +13,43 @@ addEventListener("mousemove", (event) => {
         let rotateSpeed = el.dataset.rotation;
 
         let isInLeft =  parseFloat(getComputedStyle(el).left) < innerWidth / 2 ? 1 : -1;
-        let zValue = (event.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+        let zValue = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
 
         el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) 
         perspective(2300px) translateZ(${zValue * speedz}px) rotateY(${rotateDegree * rotateSpeed}deg)`;
     })
+}
+
+update(0);
+
+addEventListener("mousemove", (event) => {
+    xValue = event.clientX - innerWidth / 2; //checks how far from middle
+    yValue = event.clientY - innerHeight / 2; //checks how far from middle
+
+    rotateDegree = (xValue / (innerWidth / 2)) * 20;
+    
+    update(event.clientX);
+})
+
+/* GSAP Animation */
+
+let timeline = gsap.timeline();
+
+// parallax_el.forEach((el) => {
+//     timeline.from(el, {
+//         // top: `${el.offsetHeight / 2 + el.dataset.distance}px`,
+//         top: `calc(50% + 90px)`,
+//         // top: '0px',
+//         duration: 1,
+//         delay: 1
+//     },
+// );
+// });
+
+timeline.to(".bg-img", {
+    // top: `${document.querySelector(".bg-img").offsetHeight / 2}px`,
+    duration: 1,
+    delay: 1,
+    top: `calc(50% + 90px)`,
+
 })
